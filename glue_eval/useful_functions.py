5dd465bc49487f567064b805e91c7bbf05a24195
golab <golab.borzooei@dataholix.de>
2025-08-11 11:15:41 +0200

    sync

--- glue_eval/useful_functions.py
+++ glue_eval/useful_functions.py
@@ -37,6 +37,7 @@ MODEL_NAME_TO_MAXIMUM_CONTEXT_LENGTH_MAP = {
     "gpt2-xl": 1024,
     "llama-2-7b-hf": 4096,
     "llama3-8b-instruct": 4096,
+    "meta-llama-3-8b-instruct": 8192,
     "eleutherai_gpt-j-6b": 2048,
     "gpt2-large": 1024,
     "gpt2-medium": 1024
