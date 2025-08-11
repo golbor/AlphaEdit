3077b70a877a3aeb4f414eee48681f4d63b781b2
golab <golab.borzooei@dataholix.de>
2025-08-11 14:58:44 +0200

    sync

--- run.sh
+++ run.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=Model-Edit
-#SBATCH --output=alpha_output.txt
-#SBATCH --time=24:00:00
-#SBATCH --gres=gpu:2
+#SBATCH --job-name=AlphaEdit
+#SBATCH --output=output.txt
+#SBATCH --time=2-00:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --export=NONE
-#SBATCH --nodelist=worker-4
 
+cd /home/stud/golab/AlphaEdit
 # activate environment
-echo "Activating virtual environment"
-source .local/bin/activate
+source .venv/bin/activate
 
-# set NLTK data directory to avoid download errors
-export NLTK_DATA="$PWD/.local/nltk_data"
-mkdir -p $NLTK_DATA
-
-sleep 5
 # run experiment
-echo "Running experiment"
-
 python3 -m experiments.evaluate     --alg_name=AlphaEdit     --model_name=meta-llama/Meta-Llama-3-8B-Instruct     --hparams_fname=Llama3-8B.json --ds_name=mcf --dataset_size_limit=2000    --num_edits=100 --downstream_eval_steps=5
